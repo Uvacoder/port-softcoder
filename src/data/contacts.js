9e693d4154513260a2d93cb5b2ebc844b95be25c
whale-watching <chenchrissh@gmail.com>
2022-11-18 08:48:51 -0800

    correct some bug

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -1,7 +1,8 @@
 const contacts = [
   {social: 'email', href: 'kennethmei811@gmail.com', text:'kennethmei811@gmail.com'},
+  {social: 'telegram', href: 'https://t.me/@Billibear', text:'@Billibear'},
   {social: 'linkedin', href: 'https://www.linkedin.com/in/kenneth-mei-633196257/', text: "in/kenneth-mei-633196257"},
-  {social: 'github', href: 'https://github.com/softtscoder', text: "/softtscoder"}
+  {social: 'github', href: 'https://github.com/softtscoder', text: "softtscoder"}
 ];
 
 export default contacts;
